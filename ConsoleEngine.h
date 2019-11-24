f85139308e93c4dfd48e73fd0c88948de43d1df9
Josh Paige <jgavinpaige@gmail.com>
2019-11-24 12:43:44 -0500

    Added functions to track key press and release

--- ConsoleEngine.h
+++ ConsoleEngine.h
@@ -10,6 +10,7 @@ private:
 	COORD m_bufferSize;
 	SMALL_RECT m_screenDimensions;
 	bool m_running;
+	bool m_keyPressed[256], m_keyDown[256], m_keyReleased[256];
 public:
 	HANDLE GetOutputHandle();
 	HANDLE GetInputHandle();
@@ -27,6 +28,10 @@ public:
 	void FillRect(int x, int y, int width, int height, char ch, int attribute);
 	void SetAll(char ch);
 	void SetAll(char ch, int attribute);
+
+	bool KeyPressed(int keyCode);
+	bool KeyDown(int keyCode);
+	bool KeyReleased(int keyCode);
 
 	char GetChar(int x, int y);
 	int GetWidth();
@@ -103,6 +108,10 @@ bool ConsoleGame::CreateConsole(int consoleWidth, int consoleHeight, int charWid
 	OnCreate();
 	WriteConsoleOutput(m_outHandle, m_screen, m_bufferSize, { 0, 0 }, &m_screenDimensions);
 
+	memset(m_keyDown, 0, sizeof(bool) * 256);
+	memset(m_keyPressed, 0, sizeof(bool) * 256);
+	memset(m_keyReleased, 0, sizeof(bool) * 256);
+
 	return true;
 }
 
@@ -164,6 +173,37 @@ void ConsoleGame::SetAll(char ch, int attribute)
 		m_screen[i].Char.AsciiChar = ch;
 		m_screen[i].Attributes = attribute;
 	}
+}
+
+bool ConsoleGame::KeyPressed(int keyCode)
+{
+	if (!m_keyDown[keyCode] && GetAsyncKeyState(keyCode) < 0)
+	{
+		m_keyDown[keyCode] = true;
+		m_keyPressed[keyCode] = true;
+	}
+	else
+		m_keyPressed[keyCode] = false;
+
+	return m_keyPressed[keyCode];
+}
+
+bool ConsoleGame::KeyDown(int keyCode)
+{
+	return GetAsyncKeyState(keyCode) < 0;
+}
+
+bool ConsoleGame::KeyReleased(int keyCode)
+{	
+	if (!m_keyReleased[keyCode] && m_keyDown[keyCode] && GetAsyncKeyState(keyCode) >= 0)
+	{
+		m_keyReleased[keyCode] = true;
+		m_keyDown[keyCode] = false;
+	}
+	else
+		m_keyReleased[keyCode] = false;
+
+	return m_keyReleased[keyCode];
 }
 
 char ConsoleGame::GetChar(int x, int y)
