0f3d45f4bb3edce13a84ba4dbd1f95fd5f2750e9
Josh Paige <jgavinpaige@gmail.com>
2019-11-26 10:37:48 -0500

    Maze generator updated to show path

--- Projects/MazeGenerator.cpp
+++ Projects/MazeGenerator.cpp
@@ -5,7 +5,7 @@
 
 struct MazeSquare
 {
-	static enum direction {UP = 0b1000, DOWN = 0b0100, LEFT = 0b0010, RIGHT = 0b0001};
+	static enum direction { UP = 0b1000, DOWN = 0b0100, LEFT = 0b0010, RIGHT = 0b0001 };
 
 	bool visited = false;
 	int direction = 0;
@@ -15,9 +15,8 @@ class Maze : public ConsoleGame
 {
 private:
 	MazeSquare* m_grid;
-	std::stack<vec::Vector2> position;
+	std::stack<vec::Vector2> position, path;
 	int m_gridWidth, m_gridHeight;
-
 public:
 	void OnCreate() override
 	{
@@ -45,6 +44,10 @@ public:
 		if (pos.Y < m_gridHeight - 1 && !m_grid[pos.X + (pos.Y + 1) * m_gridWidth].visited)
 			nextPositions.push_back({ pos.X, pos.Y + 1 });
 
+		//check if path to end
+		if (pos.X == m_gridWidth - 1 && pos.Y == m_gridHeight - 1)
+			path = position;
+
 		//pick a directions
 		if (nextPositions.size())
 		{
@@ -62,9 +65,6 @@ public:
 		}
 		else
 			position.pop();
-
-		if (!position.size())
-			Stop();
 
 		//display grid to console
 		for (int i = 0; i < m_gridWidth * m_gridHeight; i++)
@@ -84,6 +84,33 @@ public:
 			if (m_grid[i].direction & MazeSquare::RIGHT)
 				FillRect(i % m_gridWidth * 3 + 3, i / m_gridWidth * 3 + 1, 2, 2, ' ');
 		}
+
+		if (!position.size())
+		{
+			while (path.size())
+			{
+				vec::Vector2 pos = path.top();
+				path.pop();
+
+				FillRect(pos.X * 3 + 1, pos.Y * 3 + 1, 2, 2, 219, FOREGROUND_GREEN);
+
+				if (path.size())
+				{
+					if (path.top().Y < pos.Y) //up
+						FillRect(pos.X * 3 + 1, pos.Y * 3, 2, 1, 219, FOREGROUND_GREEN);
+					else if (path.top().Y > pos.Y) //down
+						FillRect(pos.X * 3 + 1, pos.Y * 3 + 3, 2, 1, 219, FOREGROUND_GREEN);
+					else if (path.top().X < pos.X) //left
+						FillRect(pos.X * 3, pos.Y * 3 + 1, 1, 2, 219, FOREGROUND_GREEN);
+					else //right
+						FillRect(pos.X * 3 + 3, pos.Y * 3 + 1, 1, 2, 219, FOREGROUND_GREEN);
+				}
+
+				//Sleep(150);
+			}
+
+			Stop();
+		}
 	}
 	void OnKeyPress(int keyCode) override {}
 	void OnKeyRelease(int keyCode) override {}
@@ -91,7 +118,7 @@ public:
 
 int main()
 {
-	Maze mazeGame;
-	mazeGame.CreateConsole(120, 80, 10, 10, ' ');
-	mazeGame.Start();
+	Maze maze;
+	maze.CreateConsole(200, 100, 8, 8, ' ');
+	maze.Start();
 }
